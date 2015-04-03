1b5c516c5743e52f9a213ac0c5349ea0f0e96c66
Jacob Coulter <jacob.coulter@leandog.com>
2015-04-02 20:46:26 -0400

    sorting date arrays

--- spec/DateUtilSpec.js
+++ spec/DateUtilSpec.js
@@ -1,15 +1,10 @@
 describe("DateUtil", function () {
-    var dateUtil;
-
-    beforeEach(function () {
-        dateUtil = new DateUtil;
-    });
 
     it("should find gaps", function () {
-        expect(dateUtil.hasGap(new Date("12/12/2010"), new Date("12/13/2010"))).toBeFalsy();
-        expect(dateUtil.hasGap(new Date("12/12/2010"), new Date("1/13/2011"))).toBeFalsy();
-        expect(dateUtil.hasGap(new Date("12/12/2010"), new Date("2/13/2011"))).toBeTruthy();
-        expect(dateUtil.hasGap(new Date("12/12/2010"), new Date("12/13/2011"))).toBeTruthy();
+        expect(hasGap(new Date("12/12/2010"), new Date("12/13/2010"))).toBeFalsy();
+        expect(hasGap(new Date("12/12/2010"), new Date("1/13/2011"))).toBeFalsy();
+        expect(hasGap(new Date("12/12/2010"), new Date("2/13/2011"))).toBeTruthy();
+        expect(hasGap(new Date("12/12/2010"), new Date("12/13/2011"))).toBeTruthy();
     });
 
     it("should measure number of months between", function () {
@@ -21,5 +16,14 @@ describe("DateUtil", function () {
         expect(monthsBetween(new Date("12/12/2010"), new Date("2/28/2011"))).toEqual(1);
         expect(monthsBetween(new Date("12/12/2010"), new Date("3/1/2011"))).toEqual(2);
         expect(monthsBetween(new Date("12/12/2010"), new Date("3/31/2011"))).toEqual(2);
-        });
+    });
+
+    it("should sort date arrays", function () {
+        var d1 = "12/12/2012";
+        var d2 = "12/13/2012";
+        var d3 = "12/14/2012";
+        var input = [d3, d1, d2];
+        var expected = [d3, d2, d1];
+        expect(sortDates(input)).toEqual(expected);
+    });
 });
--- src/DateUtil.js
+++ src/DateUtil.js
@@ -1,16 +1,20 @@
-function DateUtil() {
-}
-
-DateUtil.prototype.hasGap = function(d1, d2){
+hasGap = function (d1, d2) {
     return monthsBetween(d1, d2) > 0;
 
 };
 
- function monthsBetween(d1, d2) {
+function monthsBetween(d1, d2) {
     var months;
     months = (d2.getFullYear() - d1.getFullYear()) * 12;
     months -= d1.getMonth() + 1;
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
-}
+};
+
+function sortDates(dates) {
+    return dates.sort(function (a, b) {
+        return new Date(b) - new Date(a);
+    });
+};
+
 
