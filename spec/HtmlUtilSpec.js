7c49c6ed4f1fa922caaeca8b171873f0f3c6c750
Jacob Coulter <jacob.coulter@leandog.com>
2015-04-08 22:20:33 -0400

    Adding index updates for 'for' attribute of labels

--- spec/HtmlUtilSpec.js
+++ spec/HtmlUtilSpec.js
@@ -1,17 +1,22 @@
 describe("HtmlUtil", function () {
 
     it("should change the index portion of an id", function () {
-        expect(changeIdIndexTo($('<div id="MyId3"></div>'), 4)).toHaveAttr('id', 'MyId4');
-        expect(changeIdIndexTo($('<div id="My3Id"></div>'), 4)).toHaveAttr('id', 'My4Id');
-        expect(changeIdIndexTo($('<div id="3MyId"></div>'), 4)).toHaveAttr('id', '4MyId');
-        expect(changeIdIndexTo($('<div id="3My2Id1"></div>'), 4)).toHaveAttr('id', '4My2Id1');
+        expect(changeIndexTo($('<div id="MyId3"></div>'), 4)).toHaveAttr('id', 'MyId4');
+        expect(changeIndexTo($('<div id="My3Id"></div>'), 4)).toHaveAttr('id', 'My4Id');
+        expect(changeIndexTo($('<div id="3MyId"></div>'), 4)).toHaveAttr('id', '4MyId');
+        expect(changeIndexTo($('<div id="3My2Id1"></div>'), 4)).toHaveAttr('id', '4My2Id1');
+    });
+
+    it("should change the for attribute of a label", function () {
+        expect(changeIndexTo($('<div id="MyId3" for="Input3"></div>'), 4)).toHaveAttr('id', 'MyId4');
+        expect(changeIndexTo($('<div id="MyId3" for="Input3"></div>'), 4)).toHaveAttr('for', 'Input4');
     });
 
     it("should change the index portion of children element's ids", function () {
         var elem = $('<div id="MyId3"><p>' +
             '<label id="startDate3lbl">My Label</label>' +
             '<input type="text" id="startDate3"/></p></div>');
-        var actual = changeIdIndexTo(elem, 4);
+        var actual = changeIndexTo(elem, 4);
         expect(actual).toHaveAttr('id', 'MyId4');
         expect(actual.find('#startDate4lbl')).toExist();
         expect(actual.find('#startDate4')).toExist();
--- src/HtmlUtil.js
+++ src/HtmlUtil.js
@@ -1,11 +1,17 @@
-function changeIdIndexTo(elem, new_index) {
+function changeIndexTo(elem, new_index) {
+
+    function changeAttrIndex(attr, item, index) {
+       if (item.attr(attr)){ $(item).attr(attr, item.attr(attr).replace(/\d/, index));}
+    }
 
     function changeId(item, index) {
-        return (item.attr('id')) ? $(item).attr("id", item.attr('id').replace(/\d/, index)) : item;
+        changeAttrIndex('for', item, index);
+        changeAttrIndex('id', item, index);
     }
 
     $.each($(elem).find('*'), function (ignored_index, item) {
         changeId($(item), new_index);
     });
-    return changeId(elem, new_index);
+    changeId(elem, new_index);
+    return elem;
 }
